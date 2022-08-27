ad3927e065fec4d9e1e7ef6c8501967e09c3399a
ArieleMartins <ariele.cmmelo@gmail.com>
2022-08-27 16:43:29 -0300

    Modificando Habilidades

--- js/script.js
+++ js/script.js
@@ -1,5 +1,1 @@
-var projetos = document.getElementById('projetos');
-var list = document.getElementById('list');
-projetos.addEventListener('click', function exibir(){
-    list.classList.toggle('ativo');
-})
+
