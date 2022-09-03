2e0127b8e0ad9bef07e6f7ab594b3535647ade1c
ArieleMartins <ariele.cmmelo@gmail.com>
2022-09-03 17:45:26 -0300

    Finalizando Portfolio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ var menuclick = document.getElementById('menu')
 menuclick.addEventListener('click', function abrindoMenu(){
    menu.classList.toggle('active');
 })
+
+const elementos = document.querySelectorAll('[data-animacao]');
+const animacaoScroll = () =>{
+    const windowTop = window.scrollY + window.innerHeight * 0.35;
+    elementos.forEach((element) => {
+        if(windowTop > element.offsetTop){
+            element.classList.add("animate");
+        }else{
+            element.classList.remove('animate');
+        }
+    });
+};
+animacaoScroll();
+window.addEventListener("scroll", () =>{
+    animacaoScroll();
+})
