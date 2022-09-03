930b62f3ee3a43cf3fab2439aa1ccdfed1de349f
ArieleMartins <ariele.cmmelo@gmail.com>
2022-09-03 16:13:48 -0300

    Portfolio quase finalizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+var menu = document.getElementById('menu-mobile');
+var menuclick = document.getElementById('menu')
 
+menuclick.addEventListener('click', function abrindoMenu(){
+   menu.classList.toggle('active');
+})
